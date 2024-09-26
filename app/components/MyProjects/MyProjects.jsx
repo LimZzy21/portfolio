e40236118d593acb0f0cdbf79de4faa6822663e2
mira <rainymasster@gmail.com>
2024-09-26 15:11:59 +0300

    fead: add myproject component

---
+++ app/components/MyProjects/MyProjects.jsx
@@ -0,0 +1,39 @@
+import img from '@/app/assets/uiux.png'
+
+import Image from 'next/image'
+
+export const MyProjects = ()=>{
+    return (
+        <div className="flex flex-col text-center ">
+            <h1 className="text-5xl font-bold break-before">My Projects</h1>
+            <p className='my-14'>Lorem ipsum dolor sit amet consectetur. Imperdiet convallis blandit felis ligula <br /> aliquam venenatis fghh hgjj nisi ante.</p>
+            <div className="space-x-3">
+                <button className="px-2 py-2 rounded bg-gray-100">All</button>
+                <button className="bg-orange-500 text-white px-2 py-2 rounded">UI/UX</button>
+                <button className="px-2 py-2 rounded bg-gray-100">Website Design</button>
+                <button className="px-2 py-2 rounded bg-gray-100">App Design</button>
+                <button className="px-2 py-2 rounded bg-gray-100">Graphic Design</button>
+            </div>
+
+            <div className="flex  px-8 items-center justify-center space-x-28 pt-24">
+                <div class="  ">
+                    <Image src={img} width={449} height={500} alt="Landing Page Design" class=" h-auto" />
+                        <h4 className="text-start text-orange-500  text-xl mt-4">UI UX</h4>
+                        <h3 className="text-start text-black font-bold text-2xl">AirCalling Landing Page Design</h3>
+                </div>
+                <div class="  ">
+                    <Image src={img} width={449} height={500} alt="Landing Page Design" class=" h-auto" />
+                        <h4 className="text-start text-orange-500  text-xl mt-4">UI UX</h4>
+                        <h3 className="text-start text-black font-bold text-2xl">AirCalling Landing Page Design</h3>
+                </div>
+                <div class="text-center  ">
+                    <Image src={img} width={449} height={500} alt="Landing Page Design" class=" h-auto" />
+                        <h4 className="text-start text-orange-500  text-xl mt-4">UI UX</h4>
+                        <h3 className="text-start text-black font-bold text-2xl">AirCalling Landing Page Design</h3>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { About } from "./components/About/About";
 import { Header } from "./components/Header/Header";
 import { Navbar } from "./components/Navbar/Navbar";
+import { MyProjects } from './components/MyProjects/MyProjects'
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <Header />
       <About />
+      <MyProjects />
     </div>
   );
 }
