9210b421011546b17b5c0f695f528a57fecb808e
mira <rainymasster@gmail.com>
2024-09-26 16:38:51 +0300

    feat: create contactus component

---
+++ app/components/Contact/ContactUs.jsx
@@ -0,0 +1,28 @@
+
+
+export const ContactUs = ()=>{
+
+    return(
+        <div className="flex flex-col items-center text-center">
+            <div className="">
+                <h1 className="text-4xl font-bold">Lets Design Together</h1>
+                <p className="mb-12 mt-8">Lorem ipsum dolor sit amet consectetur. Imperdiet convallis blandit felis ligula aliquam venenatis fghh hgjj nisi ante.</p>
+                <div className="flex items-center justify-center">
+
+
+
+                    <div class="mb-6 w-[30rem]">
+                        <input type="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5 " placeholder="Enter Your Email" required />
+                    </div> 
+
+                    <div>
+                        <button className="mx-4 bg-orange-500 p-3 rounded text-white">Contact me</button>
+                    </div>
+                </div>
+                
+
+        </div>
+          
+        </div>       
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { Navbar } from "./components/Navbar/Navbar";
 import { MyProjects } from './components/MyProjects/MyProjects'
 import {Service} from '@/app/components/Service/Service'
 import { Testimonials } from "@/app/components/Testimonials/Testimonials"
+import { ContactUs } from '@/app/components/Contact/ContactUs'
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <Service />
       <MyProjects />
       <Testimonials />
+      <ContactUs />
     </div>
   );
 }
